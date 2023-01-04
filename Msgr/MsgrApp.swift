b91edd07ba550ec9ff71c6030c0d54b7cccde876
ethanvannest <ethan3vannest@gmail.com>
2023-01-03 20:03:22 -0700

    initial commit

---
+++ Msgr/MsgrApp.swift
@@ -0,0 +1,17 @@
+//
+//  MsgrApp.swift
+//  Msgr
+//
+//  Created by Ethan Vannest on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MsgrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
