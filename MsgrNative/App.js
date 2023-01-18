083cfa94d9eab909646359017957aac941adc080
ethanvannest <ethan3vannest@gmail.com>
2023-01-18 01:22:26 -0700

    made changes to screen

--- MsgrNative/App.js
+++ MsgrNative/App.js
@@ -25,7 +25,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
+import {Box} from './components/Box'
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
 const Section = ({children, title}): Node => {
@@ -55,43 +55,22 @@ const Section = ({children, title}): Node => {
 };
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <ScrollView>
+        <View>
+          <Header />
+          <Section>This is a scrollable text</Section>
+          <Section>This is a scrollable text</Section>
+          <Section>This is a scrollable text</Section>
+          <Section>This is a scrollable text</Section>
+          <Section>This is a scrollable text</Section>
+          {/* <Box /> */}
+          <Section>This is a scrollable text</Section>
+          <Section>This is a scrollable text</Section>
+          <Section>This is a scrollable text</Section>
+          <Section>This is a scrollable text</Section>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
---
+++ MsgrNative/components/Box.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Box = () => {
+        <Text>Box</Text>
+}
+
+export default Box
